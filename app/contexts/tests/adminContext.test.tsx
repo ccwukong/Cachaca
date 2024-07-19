1ad0b25e72f194af2e7f63ab7bbd8fe278864e1f
Chen Cheng <ccwukong@gmail.com>
2024-07-19 18:15:35 +0800

    test case example

---
+++ app/contexts/tests/adminContext.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react'
+import { useContext } from 'react'
+import AdminContext from '../adminContext'
+
+describe('AdminContext', () => {
+  it('should be defined', () => {
+    expect(AdminContext).toBeDefined()
+  })
+
+  it('should return correct context value', async () => {
+    const TestingComponent = () => {
+      const { navItems, storeSettings } = useContext(AdminContext)
+      return (
+        <div>
+          <p>{storeSettings!.name}</p>
+          <p>{navItems[0].title}</p>
+        </div>
+      )
+    }
+
+    render(
+      <AdminContext.Provider
+        value={{
+          navItems: [{ title: 'test link', url: '/', order: 1 }],
+          account: null,
+          storeSettings: {
+            name: 'test store',
+            logo: 'https//',
+            email: 'test@test.com',
+            phone: '+1',
+            address: null,
+            description: 'test store',
+            currency: { id: 1, name: 'US Dollar', code: 'USD', symbol: '$' },
+            publicPages: [],
+            other: null,
+            banners: null,
+          },
+        }}
+      >
+        <TestingComponent />
+      </AdminContext.Provider>,
+    )
+
+    expect(screen.getByText('test store')).toBeDefined()
+    expect(screen.getByText('test link')).toBeDefined()
+  })
+})
