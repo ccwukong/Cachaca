f47fdfdd24679527ad5053d9c5de96180f060bcc
Chen Cheng <ccwukong@gmail.com>
2024-07-30 17:59:11 +0800

    resolved conflicts

---
+++ app/themes/default/pages/storefront/tests/Cart.test.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import StoreContext from '~/contexts/storeContext'
+import Cart from '~/themes/default/pages/storefront/Cart'
+import {
+  AddressItem,
+  CategoryItem,
+  ProductPublicInfo,
+  StoreSettings,
+} from '~/types'
+import * as mocks from '~/utils/mocks'
+
+describe('Cart page component', () => {
+  test('Testing Cart page component rendering with guest checkout', async () => {
+    const mockCategories: CategoryItem[] = (
+      (await mocks.getCategories()) as CategoryItem[]
+    ).map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        slug: item.slug,
+        parentId: null,
+      }
+    })
+
+    render(
+      <StoreContext.Provider
+        value={{
+          storeSettings: (await mocks.getStoreInfo()) as StoreSettings,
+          categories: mockCategories,
+          publicPages: [],
+        }}
+      >
+        <Cart
+          addresses={(await mocks.getMockAddresses()) as AddressItem[]}
+          shippingFee="9.9"
+          allowVoucher={false}
+          allowGuestCheckout={true}
+          suggestedProducts={
+            (await mocks.getMockProducts()) as ProductPublicInfo[]
+          }
+          account={null}
+        />
+      </StoreContext.Provider>,
+      { wrapper: MemoryRouter },
+    )
+
+    expect(screen.getByText('system.my_cart_ph')).toBeDefined()
+    expect(screen.getAllByText('$9.9')).toBeDefined()
+    expect(screen.getByText('system.checkout_as_guest')).toBeDefined()
+  })
+
+  test('Testing Cart page component rendering with logged user', async () => {
+    const mockCategories: CategoryItem[] = (
+      (await mocks.getCategories()) as CategoryItem[]
+    ).map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        slug: item.slug,
+        parentId: null,
+      }
+    })
+
+    render(
+      <StoreContext.Provider
+        value={{
+          storeSettings: (await mocks.getStoreInfo()) as StoreSettings,
+          categories: mockCategories,
+          publicPages: [],
+        }}
+      >
+        <Cart
+          addresses={(await mocks.getMockAddresses()) as AddressItem[]}
+          shippingFee="9.9"
+          allowVoucher={false}
+          allowGuestCheckout={false}
+          suggestedProducts={
+            (await mocks.getMockProducts()) as ProductPublicInfo[]
+          }
+          account={{
+            id: '123',
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'test@test.com',
+          }}
+        />
+      </StoreContext.Provider>,
+      { wrapper: MemoryRouter },
+    )
+
+    expect(screen.getByText('Waterpoint')).toBeDefined()
+    expect(() => screen.getByText('system.checkout_as_guest')).toThrow(
+      'Unable to find an element',
+    )
+  })
+})
